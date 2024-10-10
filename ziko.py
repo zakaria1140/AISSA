6a6d0561501544727d1449b0cbe946d9e5535201
zakaria1140 <zakariaaissa111@gmail.com>
2024-10-10 17:28:59 +0100

    Add files via upload

---
+++ ziko.py
@@ -0,0 +1,10 @@
+A=float(input("veuilles entres la valeur A:"))
+B=float(input("veuilles entres la valeur B:"))
+SOMME= A+B
+print("la somme de A et B:" ,SOMME)
+produit=A*B
+print("la produit de A et B:" ,produit)
+defference=A-B
+print("la defference de A et B:" ,defference)
+dvision=A/B
+print("la dvision de A et B:" ,dvision)
